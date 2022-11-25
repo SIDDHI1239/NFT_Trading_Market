995ea5dfcae2813aa2a32f89e4f049d9e1437ed9
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-11-24 23:58:17 -0800

    Annotations inclusion

--- NFT_Trading_Market/src/main/java/edu/sjsu/cmpe275/nft/repos/UserRepository.java
+++ NFT_Trading_Market/src/main/java/edu/sjsu/cmpe275/nft/repos/UserRepository.java
@@ -3,9 +3,11 @@ package edu.sjsu.cmpe275.nft.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import edu.sjsu.cmpe275.nft.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	@Query("from User where email=:email")
